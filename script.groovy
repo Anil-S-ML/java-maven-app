4e11bb32b72abfc60c18aab1a8c115a1417c702e
anil <anil.kumar@xoriant.com>
2025-10-23 10:26:54 +0000

    inti

--- script.groovy
+++ script.groovy
@@ -1,14 +1,21 @@
-def buildApp(version) {
-    echo "Building the application version: ${version}"
+def buildJar() {
+    echo 'Building the application ...'
+    sh 'mvn package'
 }
 
-def testApp() {
-    echo "Testing the application ..."
+def buildImage() {
+    echo "Building the Docker image ..."
+
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t anil2469/applisting:jma-2.0 .'
+        sh 'echo $PASS | docker login -u $US --password-stdin'
+        sh 'docker push anil2469/applisting:jma-2.0'
+    }
 }
 
-def deployApp(version) {
-    echo "Deploying the application ..."
-    echo "Deploying version ${version}"
+def deployApp() {
+    echo 'Deploying the application ...'
+    // Add your deployment steps here
 }
 
 return this
