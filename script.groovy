d81747edadc5237f01131a1d168c07fe752babb0
anil <anil.kumar@xoriant.com>
2025-10-24 06:00:14 +0000

    webhook

--- script.groovy
+++ script.groovy
@@ -1,21 +1,21 @@
-def buildJar() {
-    echo 'Building the application ...'
-    sh 'mvn package'
-}
+// def buildJar() {
+//     echo 'Building the application ...'
+//     sh 'mvn package'
+// }
 
-def buildImage() {
-    echo "Building the Docker image ..."
+// def buildImage() {
+//     echo "Building the Docker image ..."
 
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t anil2469/applisting:jma-2.0 .'
-        sh 'echo $PASS | docker login -u $US --password-stdin'
-        sh 'docker push anil2469/applisting:jma-2.0'
-    }
-}
+//     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+//         sh 'docker build -t anil2469/applisting:jma-2.0 .'
+//         sh 'echo $PASS | docker login -u $US --password-stdin'
+//         sh 'docker push anil2469/applisting:jma-2.0'
+//     }
+// }
 
-def deployApp() {
-    echo 'Deploying the application ...'
-    // Add your deployment steps here
-}
+// def deployApp() {
+//     echo 'Deploying the application ...'
+//     // Add your deployment steps here
+// }
 
-return this
+// return this
