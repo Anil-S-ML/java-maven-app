82c5164ce0a1865ea104af13b81163e97d7c71c9
anil <anil.kumar@xoriant.com>
2025-11-12 16:48:01 +0000

    Add jenkins file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_version = ">= 0.12.0"
-  backend "s3" {
-    bucket = "myapp-bucket"
-    key = "myapp/state.tfstate"
-    region = "eu-west-3"
-  }
-}
+# terraform {
+#   required_version = ">= 0.12.0"
+#   backend "s3" {
+#     bucket = "myapp-bucket"
+#     key = "myapp/state.tfstate"
+#     region = "eu-west-3"
+#   }
+# }
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cider_block
